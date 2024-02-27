79d3c57861d52149a6df88533d5f1a5f90c10490
Harald Kriebisch <haraldkriebisch@LT001013.dreebitnet.local>
2024-02-27 09:22:06 +0100

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,127 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+Uint8List createMessage(String request) {
+  return Uint8List.fromList(request.codeUnits);
+}
+
+void onclick() {
+  /// Initialize SerialPort and SerialPortReader variables for later
+  late SerialPort port;
+  late SerialPortReader reader;
+
+  /// The config for the serial port created
+  /// The variables are dependent on your specific device
+  /// Usually you will have to at least update the baudRate
+  final SerialPortConfig config = SerialPortConfig();
+  config.baudRate = 9600;
+  config.parity = 0;
+  config.bits = 8;
+  config.cts = 0;
+  config.rts = 1;
+  config.dtr = 1;
+  config.stopBits = 1;
+  config.xonXoff = 3;
+
+  /// The request string send over the serial port to the device
+  /// Please update this depending on your device
+  final Uint8List message = createMessage("0010001002=?096\r");
+
+  /// Get a list of all available Ports
+  /// Select the one for your device.
+  /// Here we will just take the first one, that has "usbserial" in its name, to keep things simple.
+  List<String>? availablePorts = SerialPort.availablePorts;
+  final serialPath = availablePorts.firstWhere(
+    (element) => element.contains('usbserial'),
+    orElse: () => availablePorts.first,
+  );
+
+  /// Now that we have the serialPath i.e. the port we want we can initialize the SerialPort
+  port = SerialPort(serialPath);
+
+  /// Next we check if the port is already open, and if that is the case close it so that we can continue
+  if (port.isOpen) {
+    port.close();
+  }
+
+  /// Now we can continue opening the port
+  try {
+    /// Here we open the SerialPort for both reading and writing
+    /// The function will return true if it opened successfully and we can continue.
+    if (port.open(mode: SerialPortMode.readWrite)) {
+      /// Now that the SerialPort is opened we can create the SerialPortReader that we will use to listen to messages
+      /// send by the device
+      reader = SerialPortReader(port);
+
+      /// Only now can we update the config of our SerialPort
+      /// This is important since it would be overwritten if specified before opening the port
+      port.config = config;
+
+      /// Next we can check if the port is truly opened. This is not necessary per se but good to know.
+      if (port.isOpen) {
+        debugPrint('${port.name} opened!');
+      }
+
+      /// Now we can write a message
+      /// The write method returns the number of items send, this should be equal to the length of our message
+      if (port.write(message) == message.length) {
+        print("message send $message");
+      }
+
+      /// Here we listen to the stream of the SerialPortReader
+      /// All messages send from the device over the port to you will be received here
+      reader.stream.listen(
+          (List<int> data) {
+            print("message received $data");
+
+            /// Now we can convert the charCodes received back to a String
+            String buffer = String.fromCharCodes(data);
+            print(buffer);
+          },
+          onError: (error) => print(error),
+          onDone: () {
+            print("done with listening");
+          });
+    }
+  } on SerialPortError catch (err, _) {
+    /// In case the SerialPort threw any errors during execution we can handle them here
+    print(SerialPort.lastError);
+
+    /// To avoid blocking the port indefinitely even after the application breaks or an error is thrown
+    /// we close the port.
+    port.close();
+  }
+}
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        body: Center(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                onclick();
+              },
+              child: const Text('Click me!'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
